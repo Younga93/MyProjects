2ffde0d72ccb9e5c9c84fecb543ff2413d366f0b
Younga Jin <youngajin93@gmail.com>
2020-06-25 12:03:19 -0400

    Guess a word
    Application to play game 'guess a word' like hangman game.
    
    Younga Jin
    -This application was created on November 23, 2019.
    -Initial upload date: June 25, 2020.
    
    Co-Authored-By: Younga <younga93@users.noreply.github.com>

---
+++ GuessAWord/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuessAWord
+{
+    class Program
+    {
+        /* Younga Jin
+		 * Date: November 23, 2019
+		 */
+        static void Main(string[] args)
+        {
+            string[] words = new string[] { "programming", "software", "engineering", "centennial", "christmas", "thanksgiving", "computer", "lovelace" };
+
+            Random ranNumberGenerator = new Random();
+            int selector;
+            selector = ranNumberGenerator.Next(0, words.Length);
+
+            char[] answer = new char[words[selector].Length];
+            char[] output = new char[answer.Length];
+            for (int i = 0; i < answer.Length; i++)
+            {
+                answer[i] = words[selector][i];
+                output[i] = '*';
+            }
+
+            char userInput;
+            int endingChecker;
+            do
+            {
+                Console.Write("Word: ");
+                for (int i = 0; i < output.Length; i++)
+                {
+                    Console.Write(output[i]);
+                }
+                Console.WriteLine();
+
+                Console.Write("Guess an letter. >>");
+                userInput = Convert.ToChar(Console.ReadLine().ToLower());
+
+                bool answerMarker = false;
+                endingChecker = 0;
+                for (int i = 0; i < answer.Length; i++)
+                {
+                    if (answer[i] == userInput)
+                    {
+                        output[i] = userInput;
+                        answerMarker = true;
+                    }
+                    if (output[i] == '*')
+                    {
+                        endingChecker++;
+                    }
+                }
+
+                if (answerMarker)
+                {
+                    Console.WriteLine($"Yes! {userInput} is in the word.");
+                }
+                else
+                {
+                    Console.WriteLine($"Sorry. {userInput} is not in the word.");
+                }
+            } while (endingChecker != 0);
+            Console.WriteLine($"Congratulation! The word is '{words[selector]}'!");
+        }
+    }
+}
