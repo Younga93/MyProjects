fa4cc6ed01541d24f918bf1a213880d0e191178f
Younga Jin <youngajin93@gmail.com>
2020-06-24 23:52:16 -0400

    IsPerfect(IsPrime)
    Application to validate the number whether it is a perfect(prime) number or not.
    
    Younga Jin
    -This application was created on December 6, 2019.
    -Initial upload date: June 24, 2020.
    
    Co-Authored-By: Younga <younga93@users.noreply.github.com>

---
+++ IsPerfect/IsPerfect/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IsPerfect
+{
+    // Author: Younga Jin
+    // Date  : 05 December 2019
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int anInteger = GetNumber();
+            if (IsPerfect(anInteger))
+            {
+                Console.WriteLine($"{anInteger} is a perfect number.");
+            }
+            else
+            {
+                Console.WriteLine($"{anInteger} is not a perfect number.");
+            }
+        }
+        static bool IsPerfect(int anInteger)
+        {
+            int sum = 0;
+            bool isPerfect;
+
+            for (int i = 1; i < anInteger; i++)
+            {
+                if (anInteger % i == 0)
+                {
+                    sum += i;
+                }
+            }
+            if (sum == anInteger)
+            {
+                isPerfect = true;
+            }
+            else
+            {
+                isPerfect = false;
+            }
+            return isPerfect;
+        }
+        static int GetNumber()
+        {
+            int anInteger;
+            bool isAnTnteger;
+            do
+            {
+                Console.Write("Enter an integer: ");
+                isAnTnteger = int.TryParse(Console.ReadLine(), out anInteger);
+                if (anInteger <= 0)
+                {
+                    isAnTnteger = false;
+                }
+            } while (!isAnTnteger);
+            return anInteger;
+        }
+    }
+}
