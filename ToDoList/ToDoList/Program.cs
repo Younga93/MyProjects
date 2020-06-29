fe9fc47a78e4f881872696baa91e5d899b3c5590
Younga Jin <youngajin93@gmail.com>
2020-06-28 21:32:31 -0400

    To Do List (Consol)
    To Do List (Consol Application)
    
    Initial date: June 26, 2020
    -Adding basic task.

---
+++ ToDoList/ToDoList/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string userInput;
+            Task task;
+            do
+            {
+                Console.WriteLine("Select below option");
+                Console.WriteLine("  1 : add a task");
+                Console.WriteLine("  2 : list tasks");
+                Console.WriteLine("999 : exit program");
+                userInput = Console.ReadLine();
+
+                if (userInput == "1")
+                {
+                    task = AddTask();
+
+                    Console.WriteLine("Success to add");
+                    Console.WriteLine(task.ToString());
+                }
+                else if (userInput == "2")
+                {
+
+                }
+                else if (userInput == "999")
+                {
+                    Console.WriteLine("Good job today. Take care!");
+                }
+                else
+                {
+                    Console.WriteLine("Invalid option is entered.");
+                }
+            } while (userInput == "999");
+        }
+
+
+        static Task AddTask()
+        {
+            string name;
+            string description;
+            //DateTime dueDate;
+            //DateTime startDate = DateTime.Today;
+            Priority priority;
+
+            Console.Write("Enter the name of task : ");
+            name = Console.ReadLine();
+
+            Console.Write($"Describe {name} : ");
+            description = Console.ReadLine();
+
+            //Console.Write($"When is the due for {name}? (mm/dd/yyyy) : ");
+            //dueDate = DateTime.Parse(Console.ReadLine());
+
+            Console.Write("choose the priority (1: High/ 2: Medium/ 3: Low) : ");
+            switch (Console.ReadLine())
+            {
+                case "1":
+                    priority = Priority.High;
+                    break;
+                case "2":
+                    priority = Priority.Medium;
+                    break;
+                case "3":
+                    priority = Priority.High;
+                    break;
+                default:
+                    priority = Priority.Else;
+                    break;
+            }
+
+            //return new Task(name, description, startDate, dueDate, priority);
+            return new Task(name, description, priority);
+        }
+    }
+}
---
+++ ToDoList/ToDoList/Task.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoList
+{
+    enum Priority
+    {
+        Else,       // 0
+        High,       // 1
+        Medium,     // 2
+        Low,        // 3
+    }
+    class Task
+    {
+        //fields
+        private static int currentId = 1;
+
+        //properties
+        public int TaskId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime DueDate { get; set; }
+        public Priority Priority { get; set; }
+
+        //constructors
+        public Task(string name, string description, DateTime startDate, DateTime dueDate, Priority priority)
+        {
+            TaskId = currentId;
+            currentId++;
+            Name = name;
+            Description = description;
+            StartDate = startDate;
+            DueDate = dueDate;
+            Priority = priority;
+        }
+        public Task(string name, string description, Priority priority)
+        {
+            TaskId = currentId;
+            currentId++;
+            Name = name;
+            Description = description;
+            StartDate = DateTime.Today;
+            DueDate = DateTime.Today;
+            Priority = priority;
+        }
+        //methods
+        public override string ToString()
+        {
+            return $"currentId : {currentId}\nTaskId: {TaskId}\nName: {Name}\nDescription: {Description}\nStartDate: {StartDate}\nDueDate: {DueDate}\nPriority: {Priority}";
+        }
+    }
+}
